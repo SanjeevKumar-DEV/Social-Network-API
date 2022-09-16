b35239ff635216c049686f8509483508b618b15a
Sanjeev Kumar <sanjeevkumar@me.com>
2022-09-16 23:11:29 +1000

    get single user by Id

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -24,6 +24,21 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  // get user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v').populate('friends').populate('thoughts')
+      .then((data) => {
+        if (!data) {
+          return res.status(404).json({ message: 'User not found with this id!' });
+        }
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    },
 };
 
 module.exports = userController;
